f270bf471fc305f7d5788e8c36af1c18d3664a03
AlvinUtar <skysportsgt@gmail.com>
2024-12-27 16:41:04 +0800

    good

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
       <h1 className="head-text">
         Hello, I'm{" "}
         <span className="blue-gradient_text font-semibold drop-shadow-sm">
-          Alvinssss
+          Alvin
         </span>
       </h1>
 
